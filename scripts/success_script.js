66ab49bd5d44d1ae77bf9401e938f8231c7a01da
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2024-03-20 21:55:01 +0100

    media update

---
+++ scripts/success_script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('professional-checkmark').style.display = 'block';
+    console.log("Succeded Contacting!")
+    setTimeout(function() {
+    window.location.href = "/";
+    }, 3000);
+});
