acd571929acb4941fc10d1ca0af7574798118ca1
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2024-03-24 00:13:43 +0100

    Description Update

--- scripts/success_script.js
+++ scripts/success_script.js
@@ -5,3 +5,10 @@ document.getElementById('professional-checkmark').style.display = 'block';
     window.location.href = "/";
     }, 3000);
 });
+
+var professionalCheckbox = document.querySelector('#professional');
+var professionalInput = document.querySelector('#professional-input');
+
+if (localStorage.getItem('isProfessional') === null) {
+    localStorage.setItem('isProfessional','false');
+}
