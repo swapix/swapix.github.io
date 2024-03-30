aa3c6ec198e7e486afcf2cc630611adb8f31d328
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2024-03-30 20:11:23 +0100

    gtagger

--- scripts/index.script.js
+++ scripts/index.script.js
@@ -76,11 +76,3 @@ document.addEventListener('DOMContentLoaded', async function() {
 });
 
 
-function allConsentGranted() {
-    gtag('consent', 'update', {
-      'ad_user_data': 'granted',
-      'ad_personalization': 'granted',
-      'ad_storage': 'granted',
-      'analytics_storage': 'granted'
-    });
-    }
