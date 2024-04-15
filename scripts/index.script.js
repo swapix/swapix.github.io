9eb5baf199769ada11e736992da1b9d135c66e68
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2024-04-15 12:45:50 +0200

    cdn

--- scripts/index.script.js
+++ scripts/index.script.js
@@ -1,31 +1,38 @@
 const requestURL = "https://mutual-loved-filly.ngrok-free.app/api/v1/";
-document.addEventListener('DOMContentLoaded', async function() {
-    allConsentGranted();
-    if(getCookie("swpKey") != null && getCookie("profileID") != null){
+document.addEventListener('DOMContentLoaded', async function() 
+{
+    if(getCookie("swpKey") != null && getCookie("profileID") != null)
+    {
         document.body.classList.remove('loaded-hidden');
         document.body.classList.remove('loaded');
 
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("content-type", "application/json");
-        const requestOptions = {
+        const requestOptions = 
+        {
             method: "GET",
             headers: myHeaders,
             header: myHeaders,
             redirect: "follow"
-          };
+        };
 
        
 
         await fetch(requestURL + "trader/" + getCookie("profileID") + "?apiKey=" + getCookie("swpKey"), requestOptions)
-        .then(response => {
-            if (response.ok) {
+        .then(response => 
+        {
+            if (response.ok) 
+            {
               return response.json();
-            } else {
+            } 
+            else 
+            {
               throw new Error("Login failed: " + response.body);
             }
           })
-        .then(data => {
+        .then(data => 
+        {
             const loginButton = document.getElementById("header-account-button");
             loginButton.href = "/pages/account/"
             loginButton.innerText = "My Account"
@@ -39,37 +46,53 @@ document.addEventListener('DOMContentLoaded', async function() {
             document.getElementById("hero-welcome-big").innerHTML = "Welcome, <span class=\"swapix-text\" id=\"hero-animated-text\">"+ data.firstName + " " + data.lastName +"<span>";
             
         })
-        .catch(error => {
+        .catch(error => 
+        {
             console.error(error);
         }); 
 
-        setTimeout(() => {
+        setTimeout(() => 
+        {
             document.body.classList.add('loaded');
-            setTimeout(() => {
+            setTimeout(() => 
+            {
                 document.body.classList.add('loaded-hidden');
             }, 500);
+
         }, 1000);
     }
 
-    document.getElementById("lite-sub-button").addEventListener('click',function(){
+    document.getElementById("lite-sub-button").addEventListener('click',
+    function()
+    {
         window.location.href = '/pages/v2/auth/';
     });
-    document.getElementById("basic-sub-button").addEventListener('click',function(){
+    document.getElementById("basic-sub-button").addEventListener('click',
+    function()
+    {
         window.location.href = '/pages/v2/auth/?referal=subscriptions-home&action=sub&key=basic';
     });
-    document.getElementById("premium-sub-button").addEventListener('click',function(){
+    document.getElementById("premium-sub-button").addEventListener('click',
+    function()
+    {
         window.location.href = '/pages/v2/auth/?referal=subscriptions-home&action=sub&key=premium';
     });
-    document.getElementById('viewMoreBtn').addEventListener('click', function() {
+    document.getElementById('viewMoreBtn').addEventListener('click', 
+    function() 
+    {
         window.location.href = '/pages/features/';
     });
     
 
-    $(document).ready(function() {
+    $(document).ready(
+    function() 
+    {
         $('.hero-content').addClass('animate__animated animate__fadeInDown');
         $('.svg-animation').addClass('animate__animated animate__fadeInRight');
 
-        setTimeout(function() {
+        setTimeout(
+        function()
+        {
             $('.hero-content').removeClass('animate__animated animate__fadeInDown');
             $('.svg-animation').removeClass('animate__animated animate__fadeInRight');
         }, 2000);
